fc97d6bdce86eb9d3a93a7c8a76fb0e5e8c9fc56
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-10-11 14:07:41 +0000

    heading color changed using JS

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function on() {
     const bulbImgElement = document.getElementById('bulb');
     bulbImgElement.src = 'bulb-on.png';
+
+    const bodyElement = document.getElementById('body');
+    bodyElement.style.backgroundColor = '#ffd11a';
+
+    const containerElement = document.getElementById('container');
+    containerElement.style.backgroundColor = '#fff';
+
+    const headingElement = document.getElementById('heading');
+    headingElement.style.color = 'black';
 }
